bff0b650716bb7c3df2ba456db122b8055e0f5b5
akhil1212 <akhilgelli1141@gmail.com>
2020-07-17 15:22:40 +0530

    survey and activity proj

--- ProAcc/BL/GetQuestionary.cs
+++ ProAcc/BL/GetQuestionary.cs
@@ -119,13 +119,13 @@ namespace ProAcc.BL
             //string MyConnectionstring = "Server=123.176.34.15,8021;user id = survey;password = Admin1!;database = survey";
             //string MyConnectionstring = "Server= sql12.freemysqlhosting.net;Database=sql12353260;Uid=sql12353260;Pwd= qHCWfvIhS7;";
 
-            //string MyConnectionstring = "Data Source = 123.176.34.15;port=8021;Integrated Security=False; Initial Catalog = survey; User ID = survey; Password = Admin1!;";
-            string constr = ConfigurationManager.ConnectionStrings["mysql"].ConnectionString;
+            string MyConnectionstring = "Data Source = 123.176.34.15;port=8021;Integrated Security=False; Initial Catalog = survey; User ID = survey; Password = Admin1!;";
+            //string constr = ConfigurationManager.ConnectionStrings["mysql"].ConnectionString;
             try
             {
                 while (true)
                 {
-                    using (MySqlConnection conn = new MySqlConnection(constr))
+                    using (MySqlConnection conn = new MySqlConnection(MyConnectionstring))
                     {
 
                         MySqlDataAdapter ad = new MySqlDataAdapter("SELECT * FROM question", conn);
@@ -186,7 +186,8 @@ namespace ProAcc.BL
 
                     if (numrecord == 0)
                     {
-                        string myQuery = "Insert into  question (Id,User_ID,la_q1,la_q2.la_q3,la_q4,la_q5,la_q6,la_q7,la_q8,la_q9,la_q10,la_q11,la_q12,fq1,fq2,fq3,submitted) Values('" + p.id + "','" + p.User_ID + "','" + p.la_q1 + "','" + p.la_q2 + "','" + p.la_q3 + "','" + p.la_q4 + "','" + p.la_q5 + "','" + p.la_q6 + "','" + p.la_q7 + "','" + p.la_q8 + "','" + p.la_q9 + "','" + p.la_q10 + "','" + p.la_q11 + "','" + p.la_q12 + "','" + p.fq1 + "','" + p.fq2 + "','" + p.fq3 + "','" + p.submitted + "')";
+                        string myQuery = "Insert into  question (Id,User_ID,la_q1,la_q2,la_q3,la_q4,la_q5,la_q6,la_q7,la_q8,la_q9,la_q10,la_q11,la_q12,fq1,fq2,fq3,submitted) " +
+                            "Values('" + p.id + "','" + p.User_ID + "','" + p.la_q1 + "','" + p.la_q2 + "','" + p.la_q3 + "','" + p.la_q4 + "','" + p.la_q5 + "','" + p.la_q6 + "','" + p.la_q7 + "','" + p.la_q8 + "','" + p.la_q9 + "','" + p.la_q10 + "','" + p.la_q11 + "','" + p.la_q12 + "','" + p.fq1 + "','" + p.fq2 + "','" + p.fq3 + "','" + p.submitted + "')";
                         SqlCommand cmd2 = new SqlCommand(myQuery, con);
                         cmd2.ExecuteNonQuery();
                         con.Close();
